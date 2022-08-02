5ddb741307fd8edf8606c2d800a718e713d76b4d
EAlejandroIboy <elder.iboym@gmail.com>
2022-08-02 13:49:54 -0600

    Completed index, login, adidas and add images

---
+++ login.js
@@ -0,0 +1,5 @@
+function usrpas(){
+    if (document.form1.usuario.value=="LCP" && document.form1.pass.value=="123"){window.location="adidas.html"}
+    else {alert("Usuario o Contraseña incorrecta")}
+    }
+    document.oncontextmenu=new Function("return false");
