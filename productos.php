11988d87d9202277ba863453912e275662f510f5
EAlejandroIboy <elder.iboym@gmail.com>
2022-08-03 16:15:54 -0600

    Se implementaron nuevas pag y se termino el inicio

---
+++ productos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/icon2.png">
+    <link rel="stylesheet" href="productos.css">
+    <title>Productos / Servicios</title>
+</head>
+<body>
+    <div class="navbar">
+        <a href="adidas.html">Inicio</a> 
+        <a class="active" href="productos.php">Productos / Servicios</a> 
+        <a href="tiendas.php">Tiendas</a> 
+        <a href="contactenos.php">Contáctenos</a>
+        <a href="index.html">Login</a>
+    </div>
+</body>
+</html>
