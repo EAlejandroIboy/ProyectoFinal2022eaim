f6924f0a8058e2807b737e06725e17cacec9da7d
EAlejandroIboy <elder.iboym@gmail.com>
2022-08-04 16:16:24 -0600

    Se termino la pagina productos

--- productos.php
+++ productos.php
@@ -9,12 +9,123 @@
     <title>Productos / Servicios</title>
 </head>
 <body>
-    <div class="navbar">
-        <a href="adidas.html">Inicio</a> 
-        <a class="active" href="productos.php">Productos / Servicios</a> 
-        <a href="tiendas.php">Tiendas</a> 
-        <a href="contactenos.php">Contáctenos</a>
-        <a href="index.html">Login</a>
+
+
+    <!--Aqui comienzan los productos-->
+
+    <div class="contenedor">
+        <header>
+            <div class="navbar">
+                <a href="adidas.html">Inicio</a> 
+                <a class="active" href="productos.php">Productos / Servicios</a> 
+                <a href="tiendas.php">Tiendas</a> 
+                <a href="contactenos.php">Contáctenos</a>
+                <a href="index.html">Login</a>
+            </div>
+        </header>
+        <section class="titulo">Productos / Servicios</section>
+        <figure class="prod1">
+            <img src="images/prod1.webp">
+            <div class="capa">
+                <h3>ZAPATILLA ADIDAS SUPERSTAR X LEGO®</h3>
+                <p><b>€ 75</b></p>
+                <p>Cloud White / Pantone / Pantone</p>
+            </div>
+        </figure>
+        <figure class="prod2">
+            <img src="images/prod2.webp">
+            <div class="capa">
+                <h3>ZAPATILLA GAZELLE</h3>
+                <p><b>€ 100</b></p>
+                <p>Collegiate Navy / White / Gold Metallic</p>
+            </div>
+        </figure>
+        <figure class="prod3">
+            <img src="images/prod3.webp">
+            <div class="capa">
+                <h3>ZAPATILLA FORUM BOLD</h3>
+                <p><b>€ 110</b></p>
+                <p>Cloud White / Cloud White / Core Black</p>
+            </div>
+        </figure>
+        <figure class="prod4">
+            <img src="images/prod4.webp">
+            <div class="capa">
+                <h3>ZAPATILLA SUPERSTAR</h3>
+                <p><b>€ 70</b></p>
+                <p>Cloud White / Core Black / Blue</p>
+            </div>
+        </figure>
+        <figure class="prod5">
+            <img src="images/prod5.webp">
+            <div class="capa">
+                <h3>CAMISETA SKATEBOARDING 4.0 LOGO (GÉNERO NEUTRO)</h3>
+                <p><b>€ 33</b></p>
+                <p>Black / White</p>
+            </div>
+        </figure>
+        <figure class="prod6">
+            <img src="images/prod6.webp">
+            <div class="capa">
+                <h3>CAMISETA ADICOLOR CLASSICS 3 BANDAS</h3>
+                <p><b>€ 33</b></p>
+                <p>Night Indigo</p>
+            </div>
+        </figure>
+        <figure class="prod7">
+            <img src="images/prod7.webp">
+            <div class="capa">
+                <h3>CAMISETA ADICOLOR COLORBLOCK</h3>
+                <p><b>€ 38</b></p>
+                <p>Black / Semi Turbo / Bright Orange</p>
+            </div>
+        </figure>
+        <figure class="prod8">
+            <img src="images/prod8.webp">
+            <div class="capa">
+                <h3>BAÑADOR CUTOUT ONE-PIECE</h3>
+                <p><b>€ 85</b></p>
+                <p>Shadow Red / Medium Dark Khaki</p>
+            </div>
+        </figure>
+        <figure class="prod9">
+            <img src="images/prod9.webp">
+            <div class="capa">
+                <h3>SUDADERA CON CAPUCHA CROP</h3>
+                <p><b>€ 85</b></p>
+                <p>Off White</p>
+            </div>
+        </figure>
+        <figure class="prod10">
+            <img src="images/prod10.webp">
+            <div class="capa">
+                <h3>CALCETINES TOBILLEROS TREFOIL</h3>
+                <p><b>€ 13</b></p>
+                <p>White / Black / Medium Grey Heather</p>
+            </div>
+        </figure>
+        <figure class="prod11">
+            <img src="images/prod11.webp">
+            <div class="capa">
+                <h3>SUDADERA CUELLO REDONDO</h3>
+                <p><b>€ 90</b></p>
+                <p>Black</p>
+            </div>
+        </figure>
+        <figure class="prod12">
+            <img src="images/prod12.webp">
+            <div class="capa">
+                <h3>CONJUNTO SUDADERA Y PANTALÓN ADIDAS ORIGINALS TREFOIL</h3>
+                <p><b>€ 60</b></p>
+                <p>Almost Blue</p>
+            </div>
+        </figure>
+        <footer>
+            <p class="pbx"><b>PBX. 20222004</b></p>
+            <p class="direccion"><b>Dirección: Liceo Canadiense</b></p>
+            <p class="carne"><b>Carne: 2021-40132</b></p>
+            <p class="nombre"><b>Nombre: Elder Alejandro Iboy Montenegro</b></p>
+        </footer>
     </div>
 </body>
 </html>
