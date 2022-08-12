69ecc8ee855267b7c637fbd6f9c63dba9dee00bc
EAlejandroIboy <elder.iboym@gmail.com>
2022-08-12 10:52:24 -0600

    Tarea No.2 - Productos

--- productos.php
+++ productos.php
@@ -28,135 +28,135 @@
         </section>
         <div class="prod1" align="center">
             <figure>
-                <img src="images/prod1.webp">
+                <img src="images/producto1/prod1.webp">
                 <div class="capa">
                     <h3>ZAPATILLA ADIDAS SUPERSTAR X LEGO®</h3>
                     <p><b>€ 75</b></p>
                     <p>Cloud White / Pantone / Pantone</p>
                 </div>
             </figure>
-            <button id="informacion"><a style="text-decoration:none; color: black;" href="">Más Información</a></button>
+            <button id="informacion"><a style="text-decoration:none; color: black;" href="productos/prod1.html">Más Información</a></button>
         </div>
         <div class="prod2" align="center">
             <figure>
-                <img src="images/prod2.webp">
+                <img src="images/producto2/prod2.webp">
                 <div class="capa">
                     <h3>ZAPATILLA GAZELLE</h3>
                     <p><b>€ 100</b></p>
                     <p>Collegiate Navy / White / Gold Metallic</p>
                 </div>
             </figure>
-            <button id="informacion"><a style="text-decoration:none; color: black;" href="">Más Información</a></button>
+            <button id="informacion"><a style="text-decoration:none; color: black;" href="productos/prod2.html">Más Información</a></button>
         </div>
         <div class="prod3" align="center">
             <figure>
-                <img src="images/prod3.webp">
+                <img src="images/producto3/prod3.webp">
                 <div class="capa">
                     <h3>ZAPATILLA FORUM BOLD</h3>
                     <p><b>€ 110</b></p>
                     <p>Cloud White / Cloud White / Core Black</p>
                 </div>
             </figure>
-            <button id="informacion"><a style="text-decoration:none; color: black;" href="">Más Información</a></button>
+            <button id="informacion"><a style="text-decoration:none; color: black;" href="productos/prod3.html">Más Información</a></button>
         </div>
         <div class="prod4" align="center">
             <figure>
-                <img src="images/prod4.webp">
+                <img src="images/producto4/prod4.webp">
                 <div class="capa">
                     <h3>ZAPATILLA SUPERSTAR</h3>
                     <p><b>€ 70</b></p>
                     <p>Cloud White / Core Black / Blue</p>
                 </div>
             </figure>
-            <button id="informacion"><a style="text-decoration:none; color: black;" href="">Más Información</a></button>
+            <button id="informacion"><a style="text-decoration:none; color: black;" href="productos/prod4.html">Más Información</a></button>
         </div>
         <div class="prod5" align="center">
             <figure>
-                <img src="images/prod5.webp">
+                <img src="images/producto5/prod5.webp">
                 <div class="capa">
                     <h3>CAMISETA SKATEBOARDING 4.0 LOGO (GÉNERO NEUTRO)</h3>
                     <p><b>€ 33</b></p>
                     <p>Black / White</p>
                 </div>
             </figure>
-            <button id="informacion"><a style="text-decoration:none; color: black;" href="">Más Información</a></button>
+            <button id="informacion"><a style="text-decoration:none; color: black;" href="productos/prod5.html">Más Información</a></button>
         </div>
         <div class="prod6" align="center">
             <figure>
-                <img src="images/prod6.webp">
+                <img src="images/producto6/prod6.webp">
                 <div class="capa">
                     <h3>CAMISETA ADICOLOR CLASSICS 3 BANDAS</h3>
                     <p><b>€ 33</b></p>
                     <p>Night Indigo</p>
                 </div>
             </figure>
-            <button id="informacion"><a style="text-decoration:none; color: black;" href="">Más Información</a></button>
+            <button id="informacion"><a style="text-decoration:none; color: black;" href="productos/prod6.html">Más Información</a></button>
         </div>
         <div class="prod7" align="center">
             <figure>
-                <img src="images/prod7.webp">
+                <img src="images/producto7/prod7.webp">
                 <div class="capa">
                     <h3>CAMISETA ADICOLOR COLORBLOCK</h3>
                     <p><b>€ 38</b></p>
                     <p>Black / Semi Turbo / Bright Orange</p>
                 </div>
             </figure>
-            <button id="informacion"><a style="text-decoration:none; color: black;" href="">Más Información</a></button>
+            <button id="informacion"><a style="text-decoration:none; color: black;" href="productos/prod7.html">Más Información</a></button>
         </div>
         <div class="prod8" align="center">
             <figure>
-                <img src="images/prod8.webp">
+                <img src="images/producto8/prod8.jpg">
                 <div class="capa">
                     <h3>BAÑADOR CUTOUT ONE-PIECE</h3>
                     <p><b>€ 85</b></p>
                     <p>Shadow Red / Medium Dark Khaki</p>
                 </div>
             </figure>
-            <button id="informacion"><a style="text-decoration:none; color: black;" href="">Más Información</a></button>
+            <button id="informacion"><a style="text-decoration:none; color: black;" href="productos/prod8.html">Más Información</a></button>
         </div>
         <div class="prod9" align="center">
             <figure>
-                <img src="images/prod9.webp">
+                <img src="images/producto9/prod9.webp">
                 <div class="capa">
                     <h3>SUDADERA CON CAPUCHA CROP</h3>
                     <p><b>€ 85</b></p>
                     <p>Off White</p>
                 </div>
             </figure>
-            <button id="informacion"><a style="text-decoration:none; color: black;" href="">Más Información</a></button>
+            <button id="informacion"><a style="text-decoration:none; color: black;" href="productos/prod9.html">Más Información</a></button>
         </div>
         <div class="prod10" align="center">
             <figure>
-                <img src="images/prod10.webp">
+                <img src="images/producto10/prod10.webp">
                 <div class="capa">
                     <h3>CALCETINES TOBILLEROS TREFOIL</h3>
                     <p><b>€ 13</b></p>
                     <p>White / Black / Medium Grey Heather</p>
                 </div>
             </figure>
-            <button id="informacion"><a style="text-decoration:none; color: black;" href="">Más Información</a></button>
+            <button id="informacion"><a style="text-decoration:none; color: black;" href="productos/prod10.html">Más Información</a></button>
         </div>
         <div class="prod11" align="center">
             <figure>
-                <img src="images/prod11.webp">
+                <img src="images/producto11/prod11.webp">
                 <div class="capa">
                     <h3>SUDADERA CUELLO REDONDO</h3>
                     <p><b>€ 90</b></p>
                     <p>Black</p>
                 </div>
             </figure>
-            <button id="informacion"><a style="text-decoration:none; color: black;" href="">Más Información</a></button>
+            <button id="informacion"><a style="text-decoration:none; color: black;" href="productos/prod11.html">Más Información</a></button>
         </div>
         <div class="prod12" align="center">
             <figure>
-                <img src="images/prod12.webp">
+                <img src="images/producto12/prod12.webp">
                 <div class="capa">
                     <h3>CONJUNTO SUDADERA Y PANTALÓN ADIDAS ORIGINALS TREFOIL</h3>
                     <p><b>€ 60</b></p>
                     <p>Almost Blue</p>
                 </div>
             </figure>
-            <button id="informacion"><a style="text-decoration:none; color: black;" href="">Más Información</a></button>
+            <button id="informacion"><a style="text-decoration:none; color: black;" href="productos/prod12.html">Más Información</a></button>
         </div>
         <footer>
             <p class="pbx"><b>PBX. 20222004</b></p>
